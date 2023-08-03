dada75135aa8b7ff272ff7b671886be1678a292c
osmond <osmond@personaai.ca>
2023-08-03 15:07:46 -0700

    Email function

---
+++ src/connections/src/EmailClient.ts
@@ -0,0 +1,50 @@
+import { EmailClient, EmailMessage, KnownEmailSendStatus } from "@azure/communication-email";
+
+let emailClient: EmailClient;
+const POLLER_WAIT_TIME = 10;
+
+export const createEmailClient = () => {
+    const connectionString = process.env["COMMUNICATION_SERVICES_CONNECTION_STRING"];
+    emailClient = new EmailClient(connectionString as string);
+};
+
+export const getEmailClient = () => {
+    if (!emailClient) {
+        createEmailClient();
+    }
+    return emailClient;
+};
+
+export const sendMail = async (message: EmailMessage) => {
+    getEmailClient();
+    
+    try {
+      const poller = await emailClient.beginSend(message);
+  
+      if (!poller.getOperationState().isStarted) {
+        throw "Poller was not started.";
+      }
+  
+      let timeElapsed = 0;
+      while(!poller.isDone()) {
+        poller.poll();
+        console.log("Email send polling in progress");
+  
+        await new Promise(resolve => setTimeout(resolve, POLLER_WAIT_TIME * 1000));
+        timeElapsed += 10;
+  
+        if(timeElapsed > 18 * POLLER_WAIT_TIME) {
+          throw "Polling timed out.";
+        }
+      }
+  
+      if(poller.getResult()?.status === KnownEmailSendStatus.Succeeded) {
+        console.log(`Successfully sent the email (operation id: ${poller.getResult()?.id})`);
+      }
+      else {
+        throw poller.getResult()?.error;
+      }
+    } catch (e) {
+      console.log(e);
+    }
+};
---
+++ src/emails/src/components/email.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+/**
+ * Root email component
+ */
+export const Email = React.memo(() => {
+    return <div>Hello world!</div>
+});
