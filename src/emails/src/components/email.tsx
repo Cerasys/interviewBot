c2aa640e1e5c5e2c40f03a69d7998846a4967ae5
Nathan Lee <nathanblee@gmail.com>
2023-08-04 16:12:29 -0700

    implemented email formatter

--- src/emails/src/components/email.tsx
+++ src/emails/src/components/email.tsx
@@ -1,8 +1,52 @@
-import * as React from 'react';
+import * as React from "react";
+import { CompletionData } from "../functions/sendEmail";
 
 /**
  * Root email component
  */
-export const Email = React.memo(() => {
-    return <div>Hello world!</div>
+
+interface IEmailProps {
+  body: CompletionData;
+}
+
+export const Email = React.memo((props: IEmailProps) => {
+  return (
+    <React.Fragment>
+      <div>
+        <p>
+          <b>Candidate name:</b>
+        </p>
+        <p>
+          <b>Recruiter name:</b>
+        </p>
+        <h2>Overall Score: {props.body.score * 100}%</h2>
+        <hr />
+        <h2>Evaluation</h2>
+        {props.body.completions?.map((completion) => (
+          <div key={completion.question}>
+            <h3>QUESTION: {completion.question}</h3>
+            <p>
+              <b>Candidate answer: </b>
+              {completion.answer}
+            </p>
+            {completion.subpoints && (
+              <div>
+                <b>Subpoints made by the candidate: </b>
+                {completion.subpoints?.map((subpoint) => (
+                  <p>{subpoint}</p>
+                ))}
+              </div>
+            )}
+            <p>Explanation: {completion.explanation}</p>
+            <b>Grade: </b>
+            {completion.grade.charAt(0).toUpperCase() +
+              completion.grade.slice(1).toLowerCase()}
+          </div>
+        ))}
+        <hr />
+        <h2>Interview Transcript:</h2>
+        {props.body.transcript}
+      </div>
+    </React.Fragment>
+  );
 });
--- src/emails/src/functions/sendEmail.tsx
+++ src/emails/src/functions/sendEmail.tsx
@@ -3,50 +3,64 @@ import "dotenv/config";
 console.log("Process");
 console.log(process.env);
 
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
-import * as React from 'react';
-import * as ReactDOMServer from 'react-dom/server';
-import { Email } from '../components/email';
-import { sendMail } from '@personaai/connections'; 
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+} from "@azure/functions";
+import * as React from "react";
+import * as ReactDOMServer from "react-dom/server";
+import { Email } from "../components/email";
+import { sendMail } from "@personaai/connections";
 import { EmailMessage } from "@azure/communication-email";
 
-
-interface ISendEmailBody {
-    // Fill this section with what you want the post body to look like
-    applicantName: string;
+interface Completion {
+  subpoints: string[];
+  grade: string;
+  explanation: string;
+  question: string;
+  answer: string;
 }
 
+export interface CompletionData {
+  completions: Completion[];
+  score: number;
+  transcript: string;
+}
 
+export async function sendEmail(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  const body = (await request.json()) as CompletionData;
 
-export async function sendEmail(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    
-    // const body = await request.json();
+  const email = ReactDOMServer.renderToStaticMarkup(<Email body={body} />);
 
-    const email = ReactDOMServer.renderToStaticMarkup(<Email />)
-
-    const mail: EmailMessage = {
-        senderAddress: "Completion@298f2941-2b3a-47d9-bc86-75ad53092dce.azurecomm.net",
-        content: {
-            subject: "React Email Test",
-            html: email
+  const mail: EmailMessage = {
+    senderAddress:
+      "Completion@298f2941-2b3a-47d9-bc86-75ad53092dce.azurecomm.net",
+    content: {
+      subject: "Test Interview Report Card",
+      html: email,
+    },
+    recipients: {
+      to: [
+        {
+          address: "<nathan@personaai.ca>",
+          displayName: "Nathan",
         },
-        recipients: {
-            to: [
-                {
-                    address: "<osmond@personaai.ca>",
-                    displayName: "Osmond",
-                }
-            ],
-        },
-    };
+      ],
+    },
+  };
 
-    sendMail(mail)
-    
-    return { body: email };
-};
+  sendMail(mail);
 
-app.http('sendEmail', {
-    methods: ['POST'],
-    authLevel: 'function',
-    handler: sendEmail
+  return { body: email };
+}
+
+app.http("sendEmail", {
+  methods: ["POST"],
+  authLevel: "function",
+  handler: sendEmail,
 });
