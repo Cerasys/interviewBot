3bd7152bb2b53a41cdb19c94c4017c1b06ae13a8
Nathan Lee <nathanblee@gmail.com>
2023-08-24 18:51:22 -0700

    Transcript function

---
+++ src/api/src/persona/transcript.ts
@@ -0,0 +1,51 @@
+import { parse } from "dotenv";
+import OpenAI from "openai";
+
+let openai: OpenAI;
+
+function getOpenAiClient() {
+  if (!openai) {
+    openai = new OpenAI({
+      apiKey: process.env.OPENAI_KEY,
+    });
+  }
+  return openai;
+}
+
+export async function correctTranscript(
+  transcript: string,
+  model: "gpt-3.5-turbo-16k" | "gpt-4" = "gpt-4"
+) {
+  const completion = await getOpenAiClient().chat.completions.create({
+    model: model,
+    messages: [
+      {
+        role: "system",
+        content:
+          'The following transcript of a technical interview has lots of recording errors and stuttering due to language accents and poor audio recording. Fix them without changing the content of the interview itself. For Example "I I I use react" => "I use react"',
+      },
+      {
+        role: "user",
+        content: transcript,
+      },
+    ],
+    temperature: 0,
+    max_tokens: 4000,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  console.log(completion.choices);
+
+  const rawContent = completion.choices[0]?.message?.content;
+  const finishReason = completion.choices[0]?.finish_reason;
+
+  console.log("Finish Reason: ", finishReason);
+  if (!rawContent) {
+    throw new Error("No response from open ai");
+  }
+
+  const parsedContent = JSON.parse(rawContent);
+  return parsedContent;
+}
--- src/api/src/persona/worker.ts
+++ src/api/src/persona/worker.ts
@@ -5,94 +5,137 @@ import axios from "axios";
 import { RedisClient } from "../util/store";
 import { gradeTranscript } from "./completions";
 import { sendEmail } from "./sendEmail";
+import { correctTranscript } from "./transcript";
 
 type CompletionJob = Job<{ botId: string }>;
 
-let completionWorker: Worker<{ botId: string; }, any, string>;
+let completionWorker: Worker<{ botId: string }, any, string>;
 let completionWorkerStarted = false;
 
 const createWorker = async () => {
-    if (completionWorkerStarted) return;
+  if (completionWorkerStarted) return;
 
-    completionWorkerStarted = true;
+  completionWorkerStarted = true;
 
-    completionWorker = new Worker(CompletionQueue, async (job: CompletionJob) => {
-        const botId = job.data.botId;
+  completionWorker = new Worker(
+    CompletionQueue,
+    async (job: CompletionJob) => {
+      const botId = job.data.botId;
 
-        const persona = await Persona.findOne({ botId: botId });
+      const persona = await Persona.findOne({ botId: botId });
 
-        if (!persona) {
-            console.log("No persona with bot id found");
-            return { result: "Success" };
-        }
-        
-        const response = await axios({
-            method: "get",
-            url: `https://api.recall.ai/api/v1/bot/${botId}/transcript/`,
-            headers: {
-                "Authorization": "Token " + process.env.recallai,
-                "Accept": "application/json",
-            }
-        });
-
-        const transcriptSections: { words: { text: string, start_timestamp: number, end_timestamp: number }[], speaker: string }[] = response.data;
- 
-        const convertToSentence = ({ words, speaker }: { words: { text: string, start_timestamp: number, end_timestamp: number }[], speaker: string }) => {
-            return words.reduce((acc, word) => {
-                return acc + word.text + " ";
-            }, `${speaker}: `);
-        };
-
-        const transcriptSentences: string[] = [];
-        const transcript = transcriptSections.reduce((acc, section) => {
-            const sentence = convertToSentence(section);
-            transcriptSentences.push(sentence);
-            // eslint-disable-next-line quotes
-            return acc + " " + convertToSentence(section);
-        }, "");
-
-        console.log("transcript made");
-
-        persona.transcript = transcript;
-        await persona.save();
-
-        async function turboCompletion() {
-            console.log("Generating turbo completion");
-            const completion = await gradeTranscript(transcript, "gpt-3.5-turbo-16k");
-            console.log("Turbo completion complete, sending mail");
-            await sendEmail({
-                completions: completion.results,
-                score: completion.score,
-                transcript: transcriptSentences,
-                candidate_name: completion.candidate,
-                recruiter_name: completion.interviewer
-            }, "Turbo interview completion");
-        }
-
-        async function detailedCompletion() {
-            console.log("Generating detailed completion");
-            const completion = await gradeTranscript(transcript, "gpt-4");
-            console.log("Detailed completion complete, sending email");
-            await sendEmail({
-                completions: completion.results,
-                score: completion.score,
-                transcript: transcriptSentences,
-                candidate_name: completion.candidate,
-                recruiter_name: completion.interviewer
-            }, "Detailed interview completion");
-        }
-
-        await Promise.all([turboCompletion(), detailedCompletion()]);
-
+      if (!persona) {
+        console.log("No persona with bot id found");
         return { result: "Success" };
-    }, {
-        connection: await RedisClient()
-    });
+      }
+
+      const response = await axios({
+        method: "get",
+        url: `https://api.recall.ai/api/v1/bot/${botId}/transcript/`,
+        headers: {
+          Authorization: "Token " + process.env.recallai,
+          Accept: "application/json",
+        },
+      });
+
+      const transcriptSections: {
+        words: {
+          text: string;
+          start_timestamp: number;
+          end_timestamp: number;
+        }[];
+        speaker: string;
+      }[] = response.data;
+
+      const convertToSentence = ({
+        words,
+        speaker,
+      }: {
+        words: {
+          text: string;
+          start_timestamp: number;
+          end_timestamp: number;
+        }[];
+        speaker: string;
+      }) => {
+        return words.reduce((acc, word) => {
+          return acc + word.text + " ";
+        }, `${speaker}: `);
+      };
+
+      const transcriptSentences: string[] = [];
+      const transcript = transcriptSections.reduce((acc, section) => {
+        const sentence = convertToSentence(section);
+        transcriptSentences.push(sentence);
+        // eslint-disable-next-line quotes
+        return acc + " " + convertToSentence(section);
+      }, "");
+
+      console.log("transcript made");
+
+      var newTranscript = "";
+      async function detailedTranscript() {
+        console.log("Asking GPT for a better transcript");
+        newTranscript = await correctTranscript(transcript, "gpt-4");
+        console.log("Transcript improvement completion complete.");
+      }
+      console.log("transcript improved");
+
+      persona.transcript = newTranscript;
+      await persona.save();
+
+      async function turboCompletion() {
+        console.log("Generating turbo completion");
+        const completion = await gradeTranscript(
+          transcript,
+          "gpt-3.5-turbo-16k"
+        );
+        console.log("Turbo completion complete, sending mail");
+        await sendEmail(
+          {
+            completions: completion.results,
+            score: completion.score,
+            transcript: transcriptSentences,
+            candidate_name: completion.candidate,
+            recruiter_name: completion.interviewer,
+          },
+          "Turbo interview completion"
+        );
+      }
+
+      async function detailedCompletion() {
+        console.log("Generating detailed completion");
+        const completion = await gradeTranscript(transcript, "gpt-4");
+        console.log("Detailed completion complete, sending email");
+        await sendEmail(
+          {
+            completions: completion.results,
+            score: completion.score,
+            transcript: transcriptSentences,
+            candidate_name: completion.candidate,
+            recruiter_name: completion.interviewer,
+          },
+          "Detailed interview completion"
+        );
+      }
+
+      await Promise.all([
+        detailedTranscript(),
+        turboCompletion(),
+        detailedCompletion(),
+      ]);
+
+      return { result: "Success" };
+    },
+    {
+      connection: await RedisClient(),
+    }
+  );
 };
 
 export const startWorkerIfNotStarted = async () => {
-    if (completionWorker) {
-        return;
-    }
-    await createWorker();
+  if (completionWorker) {
+    return;
+  }
+  await createWorker();
 };
